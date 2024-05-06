faa276ad90ddd3c0bcf2789d3aadc48f9bcda0e5
root <root@d4bed9d2d86d.ub.edu.bz>
2024-05-06 16:32:42 -0600

    side nav menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 // Navbar.tsx
-import React from 'react';
+import React, { Profiler } from 'react';
 import "./Navbar.css";
 
 
@@ -10,8 +10,21 @@ interface NavbarProps {
 export const Navbar: React.FC<NavbarProps> = () => {
   return (
     <div className="navbar">
-    
-  </div>
+        <div>
+          <h1>Dashboard</h1>
+        </div>
+        <div>
+          <img src={`${Search}`} alt="" />
+          <input type="search" />
+        </div>
+        <div>
+          <img src={`${Notification}`} alt="" />
+          <div>
+            <img src={`${Profile}`} alt="" />
+            <p>James Faber</p>
+          </div>
+        </div>
+    </div>
   );
 };
 
