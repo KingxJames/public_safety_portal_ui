cddefc68aeec6e420e1be44d9073593769a5b5f2
root <root@d4bed9d2d86d.ub.edu.bz>
2024-05-07 16:47:35 -0600

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 // Navbar.tsx
 import React, { Profiler } from 'react';
 import "./Navbar.css";
-import
+import Search from "../../components/Icons/Search.png";
+import Profile from "../../components/Icons/boy.jpg"
+import Notifications from "../../components/Icons/Notifications.png"
 
 interface NavbarProps {
 
@@ -9,18 +11,18 @@ interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <div className="navbar">
+    <div className="mainNavbarContainer">
         <div>
           <h1>Dashboard</h1>
         </div>
-        <div>
-          <img src={`${Search}`}alt="" />
+        <div className="searchItemsContainer">
+          <img src={`${Search}`} className="searchIcon" alt="" />
           <input type="search" />
         </div>
-        <div>
-          <img src={`${Notification}`} alt="" />
-          <div>
-            <img src={`${Profile}`} alt="" />
+        <div className="profileItemsContainer">
+          <img src={`${Notifications}`} className="notificationIcon" alt="" />
+          <div className="profileItems">
+            <img src={`${Profile}`} className="profileIcon" alt="" />
             <p>James Faber</p>
           </div>
         </div>
